1550bf147880cd4a8f0053eec6d6726b6967a49a
Benjamin Fry <benjaminfry@me.com>
2024-03-16 10:44:05 -0700

    add CPRA and Terms of Use

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 //! SWITRS Courier is a binary for retrieving the current SWITRS DB from i-SWITRS CA CHP
 
+#![allow(clippy::print_stdout)]
+
 use std::error::Error as StdError;
 
 use clap::Parser;
 
 use switrs_courier::{gmail, Error};
+
+const I_SWITRS: &str = "https://iswitrs.chp.ca.gov/Reports/jsp/CollisionReports.jsp";
+const TERMS_OF_SERVICE: &str = include_str!("../SWITRS-TERMS-OF-USE.txt");
+const CPRA_ADVISEMENT: &str = include_str!("../CPRA-ADVISEMENT.txt");
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -15,11 +21,31 @@ struct Args {
         hide_env_values = true
     )]
     gmail_client_token: String,
+
+    /// Print the terms of service from I-SWITRS
+    #[arg(long)]
+    terms_of_service: bool,
+
+    /// Print the CPRA Advisement from I-SWITRS
+    #[arg(long)]
+    cpra_advisement: bool,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn StdError>> {
     let args = Args::parse();
+
+    if args.terms_of_service {
+        println!("Terms of Service from {I_SWITRS}:");
+        println!("{TERMS_OF_SERVICE}");
+        return Ok(());
+    }
+
+    if args.cpra_advisement {
+        println!("CPRA Advisement from {I_SWITRS}:");
+        println!("{CPRA_ADVISEMENT}");
+        return Ok(());
+    }
 
     let client_secret = &args.gmail_client_token;
 
